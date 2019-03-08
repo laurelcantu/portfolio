82a3b65f21bbbcea54669b5a452b0ae75d3b1b83
laurelcantu <48361493+laurelcantu@users.noreply.github.com>
2019-03-08 12:57:53 -0600

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -15,4 +15,28 @@ CREATE TABLE papers (
 SELECT first_name, title, grade
     FROM students
     LEFT JOIN papers
-        ON students.id = papers.student_id;     
+        ON students.id = papers.student_id;
+    
+/* Left JOIN + IFNULL */
+    
+SELECT  first_name, 
+        IFNULL(title,'Missing'),
+        IFNULL(grade,0)
+    FROM students
+    LEFT JOIN papers
+        ON students.id = papers.student_id;  
+   /* IF NULL, LEFT JOIN, AVG */
+    
+SELECT 
+        first_name, 
+        IFNULL(AVG(grade),0) AS average,
+        CASE 
+            WHEN AVG(grade) >=75 THEN 'Passing'
+            ELSE 'Failing'
+        END AS passing_status
+FROM students
+    LEFT JOIN papers
+    ON students.id = papers.student_id
+GROUP BY students.id
+ORDER BY average DESC;
+    
